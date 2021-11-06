bb39c970c57d634931f3bb8a21ce30a85b15c5f1
TurboRaptor123 <89471036+TurboRaptor123@users.noreply.github.com>
2021-11-06 13:39:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import telebot
 API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(command=["Greet"]
+@bot.message_handler(commands=["Greet"]
 
 def greet(message):
     bot.reply_to(message , "Hey , How's it going ")
